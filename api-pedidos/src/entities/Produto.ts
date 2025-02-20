a6eb6b9b36d10a1da0fb11321e61094e080f94d8
Pedro <pedrao.gabriel1@gmail.com>
2025-02-20 10:53:47 -0400

    criação da entity de Produto

---
+++ api-pedidos/src/entities/Produto.ts
@@ -0,0 +1,38 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    BeforeInsert
+  } from 'typeorm';
+  import { v4 as uuidv4 } from 'uuid';
+  
+  @Entity({ name: 'produtos' })
+  export class Produto {
+    @PrimaryGeneratedColumn()
+    id_produto: number;
+  
+    @Column({ type: 'varchar', length: 255 })
+    nome: string;
+  
+    @Column({ type: 'decimal', precision: 10, scale: 2 })
+    preco: number;
+  
+    // SKU gerado automaticamente (exemplo usando UUID)
+    @Column({ type: 'varchar', length: 36, unique: true })
+    sku: string;
+  
+    @CreateDateColumn()
+    data_criacao: Date;
+  
+    @UpdateDateColumn()
+    data_atualizacao: Date;
+  
+    @BeforeInsert()
+    generateSKU() {
+      // Você pode ajustar a lógica para gerar um SKU conforme necessário
+      this.sku = uuidv4();
+    }
+  }
+  
