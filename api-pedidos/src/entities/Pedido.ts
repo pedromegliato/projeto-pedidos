6a8360529703d38c41f8e960ff534302b3853d69
Pedro <pedrao.gabriel1@gmail.com>
2025-02-20 11:13:43 -0400

    criação do pedido entity, supondo como será o pedidoItens entiry, onde tmos a relação de um para muitos (oneTomany)

---
+++ api-pedidos/src/entities/Pedido.ts
@@ -0,0 +1,36 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    ManyToOne,
+    OneToMany,
+    JoinColumn
+  } from 'typeorm';
+
+import { PedidoItem } from './PedidoItens';
+import { Cliente } from './Clientes';
+  
+  @Entity({ name: 'pedidos' })
+  export class Pedido {
+    @PrimaryGeneratedColumn()
+    id_pedido: number;
+  
+    @Column({ type: 'date' })
+    data: Date;
+  
+    @ManyToOne(() => Cliente)
+    @JoinColumn({ name: 'id_cliente' })
+    cliente: Cliente;
+  
+    @OneToMany(() => PedidoItem, pedidoItem => pedidoItem.pedido, { cascade: true })
+    itens: PedidoItem[];
+  
+    @CreateDateColumn()
+    data_criacao: Date;
+  
+    @UpdateDateColumn()
+    data_atualizacao: Date;
+  }
+  
